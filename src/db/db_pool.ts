6d7cdbcbebfc2030e77e9c8111f7223ffd588370
mohan_0621 <mmohanc2007@gmail.com>
2026-06-08 11:51:58 +0530

    created pool function and intialised the schema

--- src/db/db_pool.ts
+++ src/db/db_pool.ts
@@ -1,12 +1,13 @@
 import pg from 'pg';
-
+import dotenv from 'dotenv';
+dotenv.config();
 // running this file first run pnpm i pg and pnpm i -D @types/pg   because it is a third party library and we need to install its types for TypeScript to work properly.
 function create_db_connection() :pg.Client {
     const db= new pg.Client({
     user:'postgres',
     host:'localhost',
     database:'enterprise_website',
-    password:'Mohan2007',
+    password:process.env.db_password,
     port:5432
 });
     return db;
